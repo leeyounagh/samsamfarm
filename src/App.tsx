ff8c9c069913b5a2de2b32274e4a2cb117698c60
[IoT1]이수연 <dudgk0216@gmail.com>
2023-04-24 16:38:59 +0900

    dosc 컴포넌트 폴더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing/Landing";
 import Login from "./pages/login/Login";
 import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer />
     </>
   );
 }
