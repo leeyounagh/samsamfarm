0d5cacc26361a43864578f789a6d160b0cc11a8c
[IoT1]이수연 <dudgk0216@gmail.com>
2023-04-24 22:02:16 +0900

    style 헤더 모달 구현중

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import * as Styled from "./header.styled";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Modal from "./modal/Modal";
 
 export default function Header() {
-  return <div>Header</div>;
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const handleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+  return (
+    <>
+      {isModalOpen ? (
+        <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+      ) : null}
+      <Styled.Layout>
+        <Styled.NavBar
+          onClick={() => {
+            handleModal();
+          }}
+        >
+          <GiHamburgerMenu size={50} />
+        </Styled.NavBar>
+        <Styled.Logo>Sam Sam Farm</Styled.Logo>
+      </Styled.Layout>
+    </>
+  );
 }
---
+++ src/components/header/modal/modal.styled.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Layout = styled.header`
+  background-color: #92a8d1;
+  width: 30vw;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  z-index: 10;
+  animation-name: modalmove;
+  animation-duration: 2s;
+  @keyframes modalmove {
+    0% {
+      transform: scale(0, 1);
+    }
+
+    100% {
+      transform: scale(1, 1);
+    }
+  }
+`;
+export const CloseIconDiv = styled.div`
+  position: absolute;
+  top: 5%;
+  left: 80%;
+  cursor: pointer;
+`;
+
+export const ModalMushroomDiv = styled.div`
+  position: absolute;
+  top: 50%;
+  left: -20%;
+`;
+
+export const ModalMenuDiv = styled.div`
+  border: 1px solid black;
+  width: 60%;
+  height: 40%;
+  position: absolute;
+  top: 18%;
+  left: 20%;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+`;
