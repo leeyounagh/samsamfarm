3ab8b95c0af68bebd2cb3c395b069fd366ff9c57
[IoT1]이수연 <dudgk0216@gmail.com>
2023-04-26 15:45:41 +0900

    dosc 라우터 재정의

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import Login from "./pages/login/Login";
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Community from "./pages/community/Community";
+import CommunityDetail from "./pages/communitydetail/CommunityDetail";
+import CommunityUpdate from "./pages/communityupdate/CommunityUpdate";
+import Mbti from "./pages/mbti/Mbti";
+import MyPage from "./pages/mypage/MyPage";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -13,6 +19,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/community:communityid" element={<CommunityDetail />} />
+        <Route path="/mbti" element={<Mbti />} />
+        <Route path="/communityupdate" element={<CommunityUpdate />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </>
