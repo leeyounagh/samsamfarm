cb498a9108f80d1fc6dcf853b7ee0838d23798f3
Hoseong On <ohs8283@naver.com>
2023-04-27 15:35:27 +0900

    헤더 사이드바 틀

---
+++ src/components/header/sidebar/sidebar.styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Aside = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 360px;
+  height: 100%;
+  background-color: gray;
+  z-index: 150;
+`;
+
+export const CloseButton = styled.div`
+  margin-top: 10px;
+  margin-left: 10px;
+`;
