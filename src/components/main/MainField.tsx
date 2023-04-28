0344c7536af6868ef3df49c517e7e7d387b73fe5
[IoT1]이수연 <dudgk0216@gmail.com>
2023-04-29 01:04:13 +0900

    feat 모달 기능 구현중

--- src/components/main/MainField.tsx
+++ src/components/main/MainField.tsx
@@ -1,33 +1,27 @@
 import React, { useState, useEffect } from "react";
 import * as Styled from "./main.styled";
-import Modal from "./modal/modal";
-import MainCharacter from "../data/mainCharacter";
+import Modal from "./modal/Modal";
+import MainCharacter from "../../data/mainCharacter";
+import { MainType } from "../../type/type";
 
 import axios from "axios";
-
-type MainType = {
-  visiter_id: number;
-  contents: string;
-  writer: string;
-  create_at: string;
-  delete_at: string;
-  plants_id: number;
-};
 
 export default function MainField() {
   const [isMainModalOpen, setIsMainModalOpen] = useState<boolean>(false);
   const [mainData, setMainData] = useState<MainType[]>([]);
+  const [userId, setUserId] = useState<number>(0);
 
   const getMainData = async () => {
     try {
       const response = await axios.get("/maintest.json");
       const data = await response.data.data;
       setMainData(data);
+      console.log(mainData);
     } catch (err) {
       console.log(err);
     }
   };
-
+  console.log("확인", userId, MainCharacter);
   useEffect(() => {
     getMainData();
   }, []);
@@ -35,51 +29,67 @@ export default function MainField() {
     <Styled.Layout>
       <Styled.BackGroundImg src="./asset/배경.png" />
 
+      <Styled.MainPlantLayout>
+        {mainData?.map((item) => {
+          return (
+            <>
+              {item?.plants_id === 1 ? (
+                <>
+                  <Styled.MainPlantImg src="./asset/씨앗.png" id="plants" />
+                </>
+              ) : item?.plants_id === 2 ? (
+                <>
+                  <Styled.MainPlantImg src="./asset/새싹.png" id="plants" />
+                </>
+              ) : item?.plants_id === 3 ? (
+                <>
+                  <Styled.MainPlantImg src="./asset/중간새싹.png" id="plants" />
+                </>
+              ) : (
+                <>
+                  <Styled.MainPlantImg src="./asset/꽃.png" id="plants" />
+                </>
+              )}
+            </>
+          );
+        })}
+      </Styled.MainPlantLayout>
+
       <Styled.FieldLayOut>
-        {MainCharacter.map((item) => {
+        {MainCharacter?.map((item) => {
           return (
             <>
               <Styled.FieldDiv id="item">
+                {item.id}
                 <Styled.TitleDiv>
                   <Styled.CharacterImg
                     src={item.img}
                     width="50%"
                     height="40%"
                   />
+
+                  <button
+                    id="button"
+                    onClick={() => {
+                      setIsMainModalOpen(!isMainModalOpen);
+                      setUserId(item.id);
+                    }}
+                  >
+                    놀러가기
+                  </button>
                 </Styled.TitleDiv>
               </Styled.FieldDiv>
             </>
           );
         })}
-        <Styled.MainPlantLayout>
-          {mainData?.map((item) => {
-            return (
-              <>
-                {item?.plants_id === 1 ? (
-                  <Styled.MainPlantImg src="./asset/씨앗.png" id="plants" />
-                ) : item?.plants_id === 2 ? (
-                  <Styled.MainPlantImg src="./asset/새싹.png" id="plants" />
-                ) : item?.plants_id === 3 ? (
-                  <Styled.MainPlantImg src="./asset/중간새싹.png" id="plants" />
-                ) : (
-                  <Styled.MainPlantImg src="./asset/꽃.png" id="plants" />
-                )}
-                <button
-                  onClick={() => {
-                    setIsMainModalOpen(!isMainModalOpen);
-                  }}
-                >
-                  놀러가기
-                </button>
-              </>
-            );
-          })}
-        </Styled.MainPlantLayout>
       </Styled.FieldLayOut>
       {isMainModalOpen ? (
         <Modal
           isMainModalOpen={isMainModalOpen}
           setIsMainModalOpen={setIsMainModalOpen}
+          mainData={mainData}
+          setMainData={setMainData}
+          userId={userId}
         />
       ) : null}
     </Styled.Layout>
---
+++ src/type/type.ts
@@ -0,0 +1,8 @@
+export interface MainType {
+  visiter_id: number;
+  contents: string;
+  writer: string;
+  create_at: string;
+  delete_at: string;
+  plants_id: number;
+}
