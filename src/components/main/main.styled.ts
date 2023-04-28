3319c348bdefb2bad3a611a96b51f97aa49a32be
[IoT1]이수연 <dudgk0216@gmail.com>
2023-04-28 20:52:11 +0900

    refact 메인페이지 리팩토링및 가데이터 이용 기능세팅

--- src/components/main/main.styled.ts
+++ src/components/main/main.styled.ts
@@ -5,6 +5,7 @@ export const Layout = styled.main`
   z-index: 10;
   height: 60%;
   position: relative;
+  border: 1px solid black;
 `;
 
 export const BackGroundImg = styled.img`
@@ -19,6 +20,7 @@ export const FieldLayOut = styled.div`
   width: 100%;
   height: 100%;
   z-index: 10;
+
   #item:nth-child(1n) {
     position: absolute;
     top: 20%;
@@ -69,128 +71,11 @@ export const FieldDiv = styled.div`
   background-size: cover;
 `;
 
-export const Character1Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 22%;
-  left: 27%;
-`;
-export const Character2Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 22%;
-  left: 38%;
-`;
-export const Character3Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 57%;
-  left: 27%;
-`;
-export const Character4Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 57%;
-  left: 38%;
-`;
-export const Character5Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 22%;
-  left: 57%;
-`;
-export const Character6Img = styled.img`
-  position: absolute;
-  top: 20%;
-  left: 68%;
-  z-index: 10;
-`;
-export const Character7Img = styled.img`
-  position: absolute;
-  top: 55%;
-  left: 68%;
-  z-index: 10;
-`;
-export const Character8Img = styled.img`
-  position: absolute;
-  top: 55%;
-  left: 57%;
-  z-index: 10;
-`;
+export const CharacterImg = styled.img``;
 
-export const Tree1Img = styled.img`
-  position: absolute;
-  top: 22%;
-  left: 34%;
-  z-index: 10;
-`;
-export const Tree2Img = styled.img`
-  position: absolute;
-  top: 57%;
-  left: 34%;
-  z-index: 10;
-`;
-export const Tree3Img = styled.img`
-  position: absolute;
-  top: 22%;
-  left: 64%;
-  z-index: 10;
-`;
-export const Tree4Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 57%;
-  left: 64%;
-  z-index: 10;
-`;
-export const Plant1Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 25%;
-  left: 30.5%;
-`;
-export const Plant2Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 24%;
-  left: 42%;
-`;
-export const Plant3Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 57%;
-  left: 41%;
-`;
-export const Plant4Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 57%;
-  left: 30%;
-`;
-export const Plant5Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 24%;
-  left: 60%;
-`;
-
-export const Plant6Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 57%;
-  left: 60%;
-`;
-export const Plant7Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 57%;
-  left: 71%;
-`;
-export const Plant8Img = styled.img`
-  position: absolute;
-  z-index: 10;
-  top: 24%;
-  left: 72%;
+export const MainPlantImg = styled.img`
+  width: 100px;
+  height: 100px;
 `;
 
 export const TitleDiv = styled.div`
@@ -202,5 +87,60 @@ export const TitleDiv = styled.div`
     left: 10px;
     width: 90%;
     height: 50%;
+  }
+`;
+
+export const MainPlantLayout = styled.div`
+  border: 1px solid black;
+
+  width: 100%;
+  height: 100%;
+  #plants:nth-child(1n) {
+    position: absolute;
+    top: 20%;
+    left: 29%;
+    z-index: 100;
+  }
+  #plants:nth-child(2n) {
+    position: absolute;
+    top: 20%;
+    left: 40%;
+    z-index: 100;
+  }
+  #plants:nth-child(3n) {
+    position: absolute;
+    top: 55%;
+    left: 29%;
+    z-index: 100;
+  }
+  #plants:nth-child(4n) {
+    position: absolute;
+    top: 55%;
+    left: 40%;
+    z-index: 100;
+  }
+  #plants:nth-child(5n) {
+    position: absolute;
+    top: 20%;
+    left: 59%;
+    z-index: 100;
+  }
+  #plants:nth-child(6n) {
+    position: absolute;
+    top: 20%;
+    left: 70%;
+    z-index: 100;
+  }
+  #plants:nth-child(7n) {
+    position: absolute;
+    top: 55%;
+    left: 70%;
+    z-index: 100;
+  }
+  #plants:nth-child(8n) {
+    position: absolute;
+    top: 55%;
+    left: 59%;
+    z-index: 100;
   }
 `;
