b64a2eaea5588c865c394ff241f1dc3b69ceec54
Hoseong On <ohs8283@naver.com>
2023-05-02 14:41:48 +0900

    커뮤니티 페이지추가 수정

--- src/pages/community/community.styled.ts
+++ src/pages/community/community.styled.ts
@@ -1,31 +1,54 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 20px;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 40px;
 `;
 
 export const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
-  th,
-  td {
-    padding: 10px;
-    border: 1px solid #ddd;
-  }
-  th {
-    background-color: #f5f5f5;
-    font-weight: bold;
-    text-align: center;
-  }
-  td {
-    text-align: left;
-  }
 `;
 
-export const TitleLink = styled.a`
-  color: #333;
-  text-decoration: none;
+export const TableHead = styled.thead`
+  background-color: #fff2d7;
+`;
+
+export const TableRow = styled.tr`
   &:hover {
-    text-decoration: underline;
+    background-color: #f5f5f5;
+  }
+  text-align: center;
+`;
+
+export const TableData = styled.td`
+  padding: 15px;
+  border: 1px solid #ddd;
+`;
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+export const PaginationButton = styled.button<{ active?: boolean }>`
+  margin: 0 5px;
+  background-color: ${({ active }) => (active ? "#ddd" : "#fff")};
+  border: 1px solid #ddd;
+  color: ${({ active }) => (active ? "#333" : "#999")};
+  padding: 5px 10px;
+  cursor: ${({ active }) => (active ? "default" : "pointer")};
+  outline: none;
+
+  &:hover:not(:disabled),
+  &:focus:not(:disabled) {
+    background-color: ${({ active }) => (active ? "#ddd" : "#f5f5f5")};
+  }
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
   }
 `;
