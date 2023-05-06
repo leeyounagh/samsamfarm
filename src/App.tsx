b32cc4dad5072617f594bb5381b6432f2c468285
[IoT1]이수연 <dudgk0216@gmail.com>
2023-05-06 11:58:35 +0900

    dosc 폴더 재세팅

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,6 @@ import CommunityUpdate from "./pages/communityupdate/CommunityUpdate";
 import Mbti from "./pages/mbti/Mbti";
 import MyPage from "./pages/mypage/MyPage";
 import Register from "./pages/register/Register";
-import Introduce from "./pages/story/introduce";
-import IntroduceReturnPage from "./pages/story/introduceReturnPage";
-import SelectPlantPage from "./pages/story/selectPlantPage";
-import PlantConfirmPage from "./pages/story/plantConfirmPage";
-import PlantConfirmReturnPage from "./pages/story/PlantConfirmReturnPage";
-import SelectMbtiPlantPage from "./pages/story/SelectMbtiPlantPage";
-import AccessPage from "./pages/story/accessPage";
-import PlantDescriptionPage from "./pages/story/plantDescriptionPage";
 
 function App() {
   return (
@@ -31,20 +23,6 @@ function App() {
         <Route path="/communityupdate" element={<CommunityUpdate />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/introduce" element={<Introduce />} />
-        <Route path="/introduceReturnPage" element={<IntroduceReturnPage />} />
-        <Route path="/selectPlantPage" element={<SelectPlantPage />} />
-        <Route path="/plantConfirmPage" element={<PlantConfirmPage />} />
-        <Route
-          path="/plantConfirmReturnPage"
-          element={<PlantConfirmReturnPage />}
-        />
-        <Route path="/selectMbtiPlantPage" element={<SelectMbtiPlantPage />} />
-        <Route path="/accessPage" element={<AccessPage />} />
-        <Route
-          path="/plantDescriptionPage"
-          element={<PlantDescriptionPage />}
-        />
       </Routes>
       <Footer />
     </>
