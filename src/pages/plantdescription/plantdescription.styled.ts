874cbaf0a8f0dc2352227f5ba6fe28f0c95935b1
Hoseong On <ohs8283@naver.com>
2023-05-16 06:48:10 +0900

    plantdescription 반응형 수정

--- src/pages/plantdescription/plantdescription.styled.ts
+++ src/pages/plantdescription/plantdescription.styled.ts
@@ -10,26 +10,59 @@ export const Layout = styled.div`
     format("woff2");
   font-weight: normal;
   font-style: normal;
+
+  @media (max-width: 768px) {
+    height: 60vh;
+  }
 `;
+
 export const FlowerDiv = styled.div`
   width: 30%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 768px) {
+    width: 50%;
+  }
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
 `;
+
 export const DescForm = styled.div`
   width: 40%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 768px) {
+    width: 70%;
+  }
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
 `;
+
 export const FlowerImg = styled.img``;
+
 export const DescInnerDiv = styled.div`
   width: 60%;
   height: 50%;
+
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
 `;
+
 export const BtnDiv = styled.div`
   width: 100%;
   height: 30%;
@@ -37,7 +70,17 @@ export const BtnDiv = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
 export const BtnLayout = styled.div`
   margin-right: 50px;
   white-space: nowrap;
+
+  @media (max-width: 768px) {
+    margin-right: 20px;
+  }
+
+  @media (max-width: 480px) {
+    margin-right: 0;
+    margin-bottom: 20px;
+  }
 `;
