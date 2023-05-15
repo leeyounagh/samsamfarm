eebf8765d15e1f4d0f4afbb21835a69e5adb708c
Hoseong On <ohs8283@naver.com>
2023-05-15 19:53:07 +0900

    board컴포넌트 분할

--- src/components/community/board/Board.tsx
+++ src/components/community/board/Board.tsx
@@ -4,8 +4,7 @@ import * as Styled from "../community.styled";
 import Pagination from "./Pagenation";
 import { Link } from "react-router-dom";
 import { RootState } from "../../../store";
-import { CommunityType } from "../../../types";
-import CommunityDetail from "../detail/CommunityDetail";
+import PostList from "./PostList";
 
 interface Post {
   id: number;
@@ -17,60 +16,6 @@ interface Post {
   updated_at: string;
 }
 
-type PostList = Post[];
-
-interface PostListProps {
-  posts: PostList;
-}
-
-const PostList: React.FC<PostListProps> = ({ posts }) => {
-  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
-  const [clickedData, setClickedData] = useState<CommunityType>({
-    id: 0,
-    title: "test",
-    content: "test contents",
-    writer: "test writer",
-    date: "2023-05-09",
-    created_at: "2023-05-09",
-    updated_at: "2023-05-09",
-  });
-  return (
-    <>
-      <Styled.Table>
-        <Styled.TableHead>
-          <Styled.TableRow>
-            <Styled.TableData>번호</Styled.TableData>
-            <Styled.TableData>제목</Styled.TableData>
-            <Styled.TableData>작성자</Styled.TableData>
-            <Styled.TableData>작성일</Styled.TableData>
-          </Styled.TableRow>
-        </Styled.TableHead>
-        <tbody>
-          {posts.map((post) => (
-            <Styled.TableRow
-              key={post.id}
-              onClick={() => {
-                setIsOpenModal(true);
-                setClickedData(post);
-              }}
-            >
-              <Styled.TableData>{post.id}</Styled.TableData>
-              <Styled.TableData>{post.title}</Styled.TableData>
-              <Styled.TableData>{post.writer}</Styled.TableData>
-              <Styled.TableData>{post.created_at}</Styled.TableData>
-            </Styled.TableRow>
-          ))}
-        </tbody>
-      </Styled.Table>
-      {isOpenModal ? (
-        <CommunityDetail
-          setIsOpenModal={setIsOpenModal}
-          clickedData={clickedData}
-        />
-      ) : null}
-    </>
-  );
-};
 function Board() {
   const communityData = useSelector((state: RootState) => {
     return state?.community;
---
+++ src/components/community/board/PostList.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import * as Styled from "../community.styled";
+import CommunityDetail from "../detail/CommunityDetail";
+import { CommunityType } from "../../../types";
+
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+  writer: string;
+  date: string;
+  created_at: string;
+  updated_at: string;
+}
+
+type PostList = Post[];
+
+interface PostListProps {
+  posts: PostList;
+}
+
+const PostList: React.FC<PostListProps> = ({ posts }) => {
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const [clickedData, setClickedData] = useState<CommunityType>({
+    id: 0,
+    title: "test",
+    content: "test contents",
+    writer: "test writer",
+    date: "2023-05-09",
+    created_at: "2023-05-09",
+    updated_at: "2023-05-09",
+  });
+  return (
+    <>
+      <Styled.Table>
+        <Styled.TableHead>
+          <Styled.TableRow>
+            <Styled.TableData>번호</Styled.TableData>
+            <Styled.TableData>제목</Styled.TableData>
+            <Styled.TableData>작성자</Styled.TableData>
+            <Styled.TableData>작성일</Styled.TableData>
+          </Styled.TableRow>
+        </Styled.TableHead>
+        <tbody>
+          {posts.map((post) => (
+            <Styled.TableRow
+              key={post.id}
+              onClick={() => {
+                setIsOpenModal(true);
+                setClickedData(post);
+              }}
+            >
+              <Styled.TableData>{post.id}</Styled.TableData>
+              <Styled.TableData>{post.title}</Styled.TableData>
+              <Styled.TableData>{post.writer}</Styled.TableData>
+              <Styled.TableData>{post.created_at}</Styled.TableData>
+            </Styled.TableRow>
+          ))}
+        </tbody>
+      </Styled.Table>
+      {isOpenModal ? (
+        <CommunityDetail
+          setIsOpenModal={setIsOpenModal}
+          clickedData={clickedData}
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default PostList;
